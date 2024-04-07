fa2b63285b3fbd4015584774d32c1b5e0e0a61b4
Onakis <166296312+Onakis@users.noreply.github.com>
2024-04-07 19:04:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".linkmenu");
+    const icon = document.querySelector(".icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
